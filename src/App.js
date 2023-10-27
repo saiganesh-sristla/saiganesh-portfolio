4d5d27961b586cfc049923cb6557f9482b32758c
saiganesh-sristla <saiganesh3108@gmail.com>
2023-10-27 19:50:57 +0530

    added about page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Home from "./components/home.js";
 import NavBar from "./components/navbar.js";
 import Socials from "./components/socials.js";
+import About from "./components/about.js";
 
 function App() {
   return (
     <div>
       <NavBar />
       <Home />
+      <About/>
 
       <Socials />
     </div>
---
+++ src/components/about.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg w-full h-full flex flex-col justify-center mx-auto px-5">
+        <div className="pb-8">
+          <p className="text-4xl font-bold border-b-4 border-gray-500 inline">About</p>
+        </div>
+
+        <p className="text-xl mt-20">
+          Hello there! My name is Saiganesh Sristla, and I'm a 20-year-old
+          second-year BSc IT student with a passion for coding and
+          problem-solving. I am deeply fascinated by the world of technology,
+          and my goal is to become a proficient full-stack developer.
+        </p>
+
+        <br />
+
+        <p className="text-xl">
+          Outside the world of codes and algorithms, Cricket and chess are my two great passions. The strategic
+          moves in chess and the thrill of the game in cricket both resonate
+          deeply with my love for problem-solving and teamwork.
+        </p>
+        <p className="text-xl">
+          Feel free to connect with me if you share similar interests or just
+          want to chat about the exciting world of technology and sports. Let's
+          collaborate, learn, and grow together!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
